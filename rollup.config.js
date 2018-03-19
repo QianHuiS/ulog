e6266135e0de8bbc16dae4e1c077436a5208c703
QianHuiS <erina86021@gmail.com>
2018-03-19 12:59:01 +0800

    bugging

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,16 @@
-import babel form 'rollup-plugin-babel';
 
-export default{
-	input: 'src/index.js',
-	output: {
-		file: 'out/ulog.js',		//打包完的輸出位置.
-		format: 'iife',				//打包的檔案類型.
-		sourcemap: 'true',		//打包的來源位置.
-	},
-	plugins: [
-		babel({
-			eclude: 'node_modules/**',
-		})
-	],
+import babel from 'rollup-plugin-babel';
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'out/ulog.js',
+    format: 'iife',
+    sourcemap: 'true',
+  },
+  plugins: [
+    babel({
+      exclude: 'node_modules/**',
+    })
+  ],
 };
